ad0ec224d69ebc1f1e0173e05348f2e8d524ec08
Bryan Woods <woods.bryan@gmail.com>
2018-03-16 17:53:00 -0700

    Adding noinline to memory::shared reallocation
    Definite improvements in emplace_back benchmarks.

--- memory/shared.hpp
+++ memory/shared.hpp
@@ -24,11 +24,13 @@ public:
 	template <typename... Args>
 	void emplace_back(Args&&... args) {
 		if (__builtin_expect(count == capacity, false)) {
-			auto resize = std::max<size_t>(16, capacity * 2);
-			auto replacement = std::shared_ptr<T>(allocator().allocate(resize), deleter(), allocator());
-			std::copy_n(std::make_move_iterator(shared_ptr.get()), count, replacement.get());
-			shared_ptr = std::move(replacement);
-			capacity = resize;
+			[&] (void) __attribute__((noinline)) {
+				auto resize = std::max<size_t>(16, capacity * 2);
+				auto replacement = std::shared_ptr<T>(allocator().allocate(resize), deleter(), allocator());
+				std::copy_n(std::make_move_iterator(shared_ptr.get()), count, replacement.get());
+				shared_ptr = std::move(replacement);
+				capacity = resize;
+			}();
 		}
 
 		allocator().construct(shared_ptr.get() + count++, std::forward<Args>(args)...);
